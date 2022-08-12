3b4a871fccfdd72cf29d38343d69336fa8515324
abyssal <83674439+abysssal@users.noreply.github.com>
2022-08-12 10:33:02 -0400

    Add files via upload

---
+++ CameraFollowPlayer.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour
+{
+    public Transform player;
+
+    // Set the camera offset Z value to -10 or lower
+    public Vector3 cameraOffset;
+    public float cameraSpeed = 0.1f;
+
+    private void Awake()
+    {
+        player = GameObject.Find("Player").GetComponent<Transform>();
+    }
+
+    void Start()
+    {
+        transform.position = player.position + cameraOffset;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 finalPosition = player.position + cameraOffset;
+        Vector3 lerpPosition = Vector3.Lerp(transform.position, finalPosition, cameraSpeed);
+        transform.position = lerpPosition;
+    }
+}
